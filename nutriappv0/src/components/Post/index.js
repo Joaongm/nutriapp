b5c8cb217ec4aa304fd611ca12469284f042928b
João <hlabeats@gmail.com>
2021-08-27 21:23:11 -0300

    comment

--- nutriappv0/src/components/Post/index.js
+++ nutriappv0/src/components/Post/index.js
@@ -1,34 +1,34 @@
-// import { useEffect, useState } from 'react';
-// import {useParams, useHistory} from 'react-router-dom';
-// import api from '../../services/api';
+import { useEffect, useState } from 'react';
+import {useParams, useHistory} from 'react-router-dom';
+import api from '../../services/api';
+// a implementar
+export default function Post() {
+    const {id} = useParams();
+    const history = useHistory();
+    //navegação interativa
 
-// export default function Post() {
-//     const {id} = useParams();
-//     const history = useHistory();
-//     //navegação interativa
+    const [post, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-//     const [post, setPosts] = useState([]);
-//     const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        async function loadPosts() {
+            const response = await api.get(`rn-api/?api=posts/${id}`);
 
-//     useEffect(() => {
-//         async function loadPosts() {
-//             const response = await api.get(`rn-api/?api=posts/${id}`);
+            if (response.data.lenght === 0){
 
-//             if (response.data.lenght === 0){
+                history.replace('/')
+                return;
+            }
 
-//                 history.replace('/')
-//                 return;
-//             }
-
-//             setPosts(response.data);
-//             setLoading(false);
-
-
-//         }
-//         LoadPosts();
-//     }, [history, id]);
+            setPosts(response.data);
+            setLoading(false);
 
 
-// }
+        }
+        loadPosts();
+    }, [history, id]);
+
+
+};
 
 
